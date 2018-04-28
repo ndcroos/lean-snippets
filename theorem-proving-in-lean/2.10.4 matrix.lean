0f8b478e15bfbf435c97cbe21e9bdcfe54ef94ab
ndcroos <ndcroos@users.noreply.github.com>
2018-04-29 00:21:47 +0200

    Update 2.10.4 matrix.lean

--- theorem-proving-in-lean/2.10.4 matrix.lean
+++ theorem-proving-in-lean/2.10.4 matrix.lean
@@ -1,3 +1,14 @@
 /-
 Similarly, declare a constant matrix so that matrix α m n could represent the type of m by n matrices. Declare some constants to represent functions on this type, such as matrix addition and multiplication, and (using vec) multiplication of a matrix by a vector. Once again, declare some variables and check some expressions involving the constants that you have declared.
 -/
+
+universe u
+constant mat : Type u → ℕ → Type u
+
+namespace mat
+  constant add :
+    Π (α : Type u) (m : ℕ) (n : ℕ), mat α m n → mat α m n → mat α m n
+  /- Multiply matrix with a vector. -/
+  const mult :
+    Π (α : Type u) (m : ℕ) (n : ℕ), mat α m n →  vec α n → vec α b
+end vec
