a6deb7d1aa86c87b4243caa37a82cd7d361729c2
ndcroos <ndcroos@users.noreply.github.com>
2018-05-01 19:39:05 +0200

    Update 2.10.3 vector.lean

--- theorem-proving-in-lean/2.10.3 vector.lean
+++ theorem-proving-in-lean/2.10.3 vector.lean
@@ -1,5 +1,9 @@
 /-
-Above, we used the example vec α n for vectors of elements of type α of length n. Declare a constant vec_add that could represent a function that adds two vectors of natural numbers of the same length, and a constant vec_reverse that can represent a function that reverses its argument. Use implicit arguments for parameters that can be inferred. Declare some variables and check some expressions involving the constants that you have declared.
+Above, we used the example vec α n for vectors of elements of type α of length n. 
+Declare a constant vec_add that could represent a function that adds two vectors of natural numbers of the same length, 
+and a constant vec_reverse that can represent a function that reverses its argument. 
+Use implicit arguments for parameters that can be inferred. 
+Declare some variables and check some expressions involving the constants that you have declared.
 -/
 
 universe u
