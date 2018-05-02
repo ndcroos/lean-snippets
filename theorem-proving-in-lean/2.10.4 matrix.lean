f7cd1d006d9887fbbca24cea0c876ae878b9ff49
ndcroos <ndcroos@users.noreply.github.com>
2018-05-02 06:44:20 +0200

    Update 2.10.4 matrix.lean

--- theorem-proving-in-lean/2.10.4 matrix.lean
+++ theorem-proving-in-lean/2.10.4 matrix.lean
@@ -1,5 +1,6 @@
 /-
-Similarly, declare a constant matrix so that matrix α m n could represent the type of m by n matrices. Declare some constants to represent functions on this type, such as matrix addition and multiplication, and (using vec) multiplication of a matrix by a vector. Once again, declare some variables and check some expressions involving the constants that you have declared.
+Similarly, declare a constant matrix so that matrix α m n could represent the type of m by n matrices. 
+Declare some constants to represent functions on this type, such as matrix addition and multiplication, and (using vec) multiplication of a matrix by a vector. Once again, declare some variables and check some expressions involving the constants that you have declared.
 -/
 
 universe u
